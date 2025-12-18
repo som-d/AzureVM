b4f1b875ffd2e7fb9d8ce48357ef2b86c724b131
Soham Deshmukh <68391321+som-d@users.noreply.github.com>
2025-12-19 01:38:50 +0530

    pp
    Removed sensitive Azure credentials from provider.tf

--- hulk/Devops-Lab/Terraform/terraform-azure-vm/provider.tf
+++ hulk/Devops-Lab/Terraform/terraform-azure-vm/provider.tf
@@ -3,8 +3,8 @@ provider "azurerm" {
     
   }
 
-  subscription_id = "08062aa4-07e5-4135-95e4-b77d46c5ee8b"
-  client_id = "1121cd92-3987-4d27-991d-eb9385d14b94"
-  client_secret = "RMo8Q~uKPwLwwh4z7lXIaxdmYPhBGQ.jtuu1_amu"
-  tenant_id = "a36ffd61-ecba-4862-a617-5490fab6c2ad"
+  subscription_id = 
+  client_id = 
+  client_secret = 
+  tenant_id = 
 }
